17e634055a60075e964e619205a943a33068ceda
pquinn <phillmatic19@gmail.com>
2015-04-26 23:14:55 -0400

    added content, added start script, set up route

--- phil.py
+++ phil.py
@@ -1,8 +1,9 @@
 import os
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
