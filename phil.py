aa0a60dabc3bdeba2858b65122dc83c735abbc58
pquinn <phillmatic19@gmail.com>
2015-04-26 23:24:53 -0400

    added /n redirect

--- phil.py
+++ phil.py
@@ -1,9 +1,13 @@
 import os
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
+def slashn():
+    return redirect('/n')
+
+@app.route('/n')
 def index():
     return render_template('index.html')
