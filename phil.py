609f61472572cd5861f182bad2f869d6d7c52fdc
pquinn <phillmatic19@gmail.com>
2015-12-20 21:10:59 -0500

    rewindr initial

--- phil.py
+++ phil.py
@@ -1,8 +1,11 @@
 import os
 from flask import Flask
 from flask import render_template, redirect
+from rewindr import *
 
 app = Flask(__name__)
+
+app.debug = True
 
 @app.route('/')
 def slashn():
@@ -11,3 +14,16 @@ def slashn():
 @app.route('/n')
 def index():
     return render_template('index.html')
+
+@app.route('/rewindr')
+def rewindr():
+    artists = get_user_top_artists()
+    return render_template('rewindr/index.html', artists=artists)
+
+@app.route('/rewindr/today')
+@app.route('/rewindr/today/<username>')
+def rewinder_day(username="phillmatic19"):
+    tracks = get_user_recent_tracks(username)
+    return render_template('rewindr/playlist.html', tracks=tracks)
+
+if __name__ == '__main__': app.run();
---
+++ rewindr.py
@@ -0,0 +1,63 @@
+import pylast
+from datetime import date, datetime
+
+# TODO: add these to environment before committing
+API_KEY = "7af493837dee39d985a7285c4a121cd6"
+API_SECRET = "2318d1ad3b1a07b3635c5254d64d84c5"
+
+# need to authenticate to write
+username = "phillmatic19"
+#password_hash = pylast.md5("password here")
+
+network = pylast.LastFMNetwork(api_key = API_KEY, api_secret = API_SECRET)
+
+def get_top_artists():
+    return network.get_top_artists()
+
+def get_user_top_albums():
+    return network.get_user(username).get_top_albums()
+
+def get_user_top_artists():
+    return network.get_user(username).get_top_artists()
+
+def get_user_recent_tracks(username="phillmatic19"):
+    from_date = to_timestamp(one_year_ago_today())
+    to_date = to_timestamp(one_year_ago_plus_one_day())
+    #print "from_date", from_date
+    #print "to_date", to_date
+    return network.get_user(username).get_recent_tracks(limit=None, time_from=from_date, time_to=to_date)
+
+def one_year_ago_today():
+    now = datetime.now()
+    return subtract_years(now, 1)
+
+def one_year_ago_plus_one_day():
+    now = datetime.now()
+    return add_days(one_year_ago_today(), 1)
+
+def subtract_years(d, years):
+    """Return a date that's `years` years after the date (or datetime)
+    object `d`. Return the same calendar date (month and day) in the
+    destination year, if it exists, otherwise use the following day
+    (thus changing February 29 to March 1).
+
+    """
+    try:
+        return d.replace(year = d.year - years, hour=0, minute=0, second=0, microsecond=0)
+    except ValueError:
+        return d - (date(d.year - years, 1, 1) - date(d.year, 1, 1))
+
+def add_days(d, days):
+    """Return a date that's `days` days after the date (or datetime)
+    object `d`. Return the same calendar date (month and day) in the
+    destination date, if it exists, otherwise use the following day
+    (thus changing February 29 to March 1).
+
+    """
+    try:
+        return d.replace(day = d.day + days, hour=0, minute=0, second=0, microsecond=0)
+    except ValueError:
+        return d + (date(d.day + days, 1, 1) - date(d.day, 1, 1))
+
+def to_timestamp(d):
+    return (d - datetime(1970, 1, 1)).total_seconds()
